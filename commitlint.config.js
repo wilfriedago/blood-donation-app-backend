ec69c67117bf0fce5d6079f387542e87e588c9b8
Wilfried AGO <wilfriedago@pm.me>
2023-01-15 23:20:21 +0100

    docs: update commitlint.config.js with useful's comments

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,22 @@
-module.exports = { extends: ['@commitlint/config-angular'] };
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // Add a new feature
+        'fix', // Fix a bug
+        'docs', // Update documentation
+        'style', // Update code style
+        'refactor', // Refactor code
+        'perf', // Improve performance
+        'test', // Add or update tests
+        'build', // Update build system or dependencies
+        'ci', // Update CI configuration
+        'chore', // Update build process or auxiliary tools
+        'revert', // Revert to a commit
+      ],
+    ],
+  },
+};
