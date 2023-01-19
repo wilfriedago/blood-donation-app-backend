93793664ba50fb70cb155476a73d58bbd0706441
Wilfried AGO <wilfriedago@pm.me>
2023-01-19 22:25:31 +0100

    fix(ci): update city model

--- src/models/cities/cities.controller.ts
+++ src/models/cities/cities.controller.ts
@@ -1,43 +1,95 @@
 import {
   Body,
   Controller,
+  DefaultValuePipe,
   Delete,
   Get,
+  HttpCode,
+  HttpStatus,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
+  Query,
+  SerializeOptions,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+
+import { Roles } from '@/roles/roles.decorator';
+import { RoleEnum } from '@/roles/roles.enum';
+import { RolesGuard } from '@/roles/roles.guard';
+import { infinityPagination } from '@/utils/infinity-pagination';
 
 import { CitiesService } from './cities.service';
 import { CreateCityDto } from './dto/create-city.dto';
 import { UpdateCityDto } from './dto/update-city.dto';
 
-@Controller('cities')
+@ApiBearerAuth()
+@Roles(RoleEnum.admin)
+@UseGuards(AuthGuard('jwt'), RolesGuard)
+@ApiTags('Cities')
+@Controller({
+  path: 'cities',
+  version: '1',
+})
 export class CitiesController {
   constructor(private readonly citiesService: CitiesService) {}
 
+  @SerializeOptions({
+    groups: ['admin'],
+  })
   @Post()
-  create(@Body() createCityDto: CreateCityDto) {
-    return this.citiesService.create(createCityDto);
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createProfileDto: CreateCityDto) {
+    return await this.citiesService.create(createProfileDto);
   }
 
+  @SerializeOptions({
+    groups: ['admin'],
+  })
   @Get()
-  findAll() {
-    return this.citiesService.findAll();
+  @HttpCode(HttpStatus.OK)
+  async findAll(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+  ) {
+    if (limit > 50) limit = 50;
+
+    return infinityPagination(
+      await this.citiesService.findManyWithPagination({ page, limit }),
+      { page, limit },
+    );
   }
 
+  @SerializeOptions({
+    groups: ['admin'],
+  })
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.citiesService.findOne(+id);
+  @HttpCode(HttpStatus.OK)
+  async findOne(@Param('id') id: string) {
+    return await this.citiesService.findOne({ id: +id });
   }
 
+  @SerializeOptions({
+    groups: ['admin'],
+  })
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCityDto: UpdateCityDto) {
-    return this.citiesService.update(+id, updateCityDto);
+  @HttpCode(HttpStatus.OK)
+  async update(
+    @Param('id') id: string,
+    @Body() updateProfileDto: UpdateCityDto,
+  ) {
+    return await this.citiesService.update(+id, updateProfileDto);
   }
 
+  @SerializeOptions({
+    groups: ['admin'],
+  })
   @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.citiesService.softDelete(id);
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async remove(@Param('id') id: number) {
+    await this.citiesService.softDelete(+id);
   }
 }
